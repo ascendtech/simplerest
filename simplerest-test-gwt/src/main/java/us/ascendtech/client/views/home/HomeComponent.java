c6dc2a366cf5f18c2565ec538520984549c9dd2f
Matthew Davis <matt.davis@ascend-tech.us>
2021-01-13 08:28:46 -0500

    make example simple

--- simplerest-test-gwt/src/main/java/us/ascendtech/client/views/home/HomeComponent.java
+++
@@ -1,8 +0,0 @@
-package us.ascendtech.client.views.home;
-
-import com.axellience.vuegwt.core.annotations.component.Component;
-import com.axellience.vuegwt.core.client.component.IsVueComponent;
-
-@Component
-public class HomeComponent implements IsVueComponent {
-}
